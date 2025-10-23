d00c1069336a77dca6e32966d26bff24e8ba63fe
Manish Kumar <manish8193892@gmail.com>
2025-10-24 01:04:12 +0530

    Project Completed

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+exports.protect = async (req, res, next) => {
+    let token = req.headers.authorization.split(' ')[1];
+
+    // Check if token is provided in headers
+    if (!token) return res.status(401).json({ message: 'Not authorized, no token' });
+
+    try {
+        // Verify token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        
+        // Find user by ID
+        req.user = await User.findById(decoded.id).select('-password');
+        
+        if (!req.user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        
+        next(); // Proceed to the next middleware or route handler
+    } catch (error) {
+        res.status(401).json({ message: 'Not authorized, token failed' });
+    }
+}
